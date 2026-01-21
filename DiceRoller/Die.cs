3e0d7ff8e57876fa33c071f4a7291bcc2732a676
Raymond Hampson III <rayhampson4@gmail.com>
2026-01-20 18:32:48 -0800

    Added Roll() method call to the constructor so the die starts with a random value

--- DiceRoller/Die.cs
+++ DiceRoller/Die.cs
@@ -26,6 +26,9 @@ public class Die
                 $"{nameof(numSides)} must be greater than 0 and less than 21");
         }
         NumberOfSides = numSides;
+
+        // Forces the die to start as a random number
+        Roll();
     }
     /// <summary>
     /// Gets the number of sides for the die.
